ff69c2f9fd3660462b47ba352f13ac22d735f235
Ravi Kaja <RKaja@tellurianinc.com>
2022-10-18 18:28:23 +0000

    Deleted DATABASES

--- DATABASES/AZU-SQL-01/CompSurveyArchive/FullTextIndexes.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE FULLTEXT INDEX ON [dbo].[Combined_Indexed]
-    ([Job Title] LANGUAGE 1033, [UID] LANGUAGE 1033)
-    KEY INDEX [ndx_Combined_Indexed_Unique_Index]
-    ON [FullTextCatalog];
-
