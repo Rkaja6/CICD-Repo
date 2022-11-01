77fd70a968b2d6a6f8af6c05a86f6afb392f519e
Ravi Kaja <RKaja@tellurianinc.com>
2022-11-01 14:34:35 -0500

    New Files

--- AZURE/Test Table/Test Table/dbo.Test_Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Test_Table_azure]
-(
-
-	[Testkey] INT NOT NULL ,
-	[TestName] varchar (100) NULL,
-	constraint [Test_table_azurePK] primary key clustered ([TestKey] ASC)
-);
-go
